a9eb0d49f4289903cf02f65b88fdd6d1e24bc42b
Emil Voltaire <emil.voltaire@54u.se>
2019-10-09 16:43:23 +0200

    First commit

---
+++ src/renderers/server.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import StarMatch from 'components/App';
+
+export async function serverRenderer() {
+  const initialData = {
+    appName: 'Reactful',
+  };
+
+  const pageData = {
+    title: `Hello ${initialData.appName}`,
+  };
+
+  return Promise.resolve({
+    initialData,
+    initialMarkup: ReactDOMServer.renderToString(
+      <StarMatch />
+    ),
+    pageData,
+  });
+}
